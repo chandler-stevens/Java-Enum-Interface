ddbd13a9ff4f970275bb53699033b41cbc212f4c
Chandler Stevens <chandlerstevens22@gmail.com>
2018-12-02 15:11:18 -0800

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"enuminterface","l":"EnumInterfaceTest"},{"p":"enuminterface","l":"Hello"},{"p":"enuminterface","l":"PetPreference.Pet"},{"p":"enuminterface","l":"PetPreference"},{"p":"enuminterface","l":"VehiclePreference.Vehicle"},{"p":"enuminterface","l":"VehiclePreference"},{"p":"enuminterface","l":"Welcome"}]
